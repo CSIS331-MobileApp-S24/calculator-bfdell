d816ca12b3f10d962ad4953e22913401015247b9
bfdell <bf08dell@siena.edu>
2024-02-27 12:30:53 -0500

    finished calculator part 2

--- src/components/Wrapper.js
+++ src/components/Wrapper.js
@@ -1,145 +1,161 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './Wrapper.css'
 import Screen from './Screen.js'
 import ButtonBox from './ButtonBox.js'
 
-const Wrapper = () => {
-    let [screenVal, setScreenVal] = useState(0);
-    let [operator, setOperator] = useState("");
-    let [operand, setOperand] = useState(0);
-    let [resultShowing, setResultShowing] = useState(true);
-
-
-    //TODO: PERCENT, DECIMAL, AND DECIMAL THAT GOES ON FOREVER
-    const updateOperand = () => {
-        let newOperand = operand;
-        switch (operator) {
-            case "+":
-                newOperand = operand + screenVal;
-                break;
-            case "-":
-                newOperand = operand - screenVal;
-                break;
-            case "/":
-                newOperand = operand / screenVal;
-                break;
-            case "X":
-                newOperand = operand * screenVal;
-                break;
-            default:
-        }
-        return newOperand;
+class Wrapper extends React.Component {
+    operator = "";
+    operand = 0;
+    resultShowing = true;
+    decimalStart = false;
+    constructor() {
+        super();
+        this.state = { screenVal: 0 };
     }
 
-    const resetClickHandler = () => {
-        console.log("\tCalculator reset");
-        setScreenVal(0);
-        setOperand(0);
-        setOperator("");
-        setResultShowing(true);
-    };
-
-    const invertClickHandler = () => {
-        console.log("\tnumber inverted");
-        setScreenVal(screenVal * -1);
-    };
-
-    const percentClickHandler = () => {
-        console.log("\tconverting to percent");
-        setScreenVal(screenVal / 100);
-
-    };
-
-    const equalClickHandler = () => {
-        console.log("\tCalculating " + operand + " " + operator + " " + screenVal);
-
-        if (!resultShowing) {
-            let displayVal = operator === "" ? screenVal : updateOperand();
-            setScreenVal(displayVal);
-            setResultShowing(true);
-        }
-        setOperator("");
-        setOperand(0);
-    };
-
-    const decClickHandler = () => {
-        console.log("\tMaking number a decimal");
-        let screenValStr = screenVal.toString() + ".0";
-        setScreenVal(Number(screenValStr));
-
-    };
-
-    const operClickHandler = (oper) => {
-        console.log("\tHandling " + oper + " operator");
-
-        if (!resultShowing) {
-            let displayVal = operator === "" ? 0 : updateOperand();
-            setScreenVal(displayVal);
-            setResultShowing(true);
-            setOperator(oper);
-
-            if (operand === 0) {
-                setOperand(screenVal);
-            } else {
-                setOperand(displayVal);
+    render() {
+        const updateOperand = () => {
+            let newOperand = this.operand;
+            switch (this.operator) {
+                case "+":
+                    newOperand = this.operand + this.state.screenVal;
+                    break;
+                case "-":
+                    newOperand = this.operand - this.state.screenVal;
+                    break;
+                case "/":
+                    newOperand = this.operand / this.state.screenVal;
+                    break;
+                case "X":
+                    newOperand = this.operand * this.state.screenVal;
+                    break;
+                default:
             }
-        }
-    };
-
-    const numClickHandler = (num) => {
-        console.log("\t" + num + " clicked")
-        let screenValStr = screenVal.toString();
-        if (resultShowing) {
-            console.log("\tstarting new number");
-            //setting first num
-            setScreenVal(Number(num));
-            setResultShowing(false);
-        } else {
-            //appending num
-            setScreenVal(Number(screenValStr + num));
-            console.log("\tadding to num");
-        }
-    };
-
-    const onButtonClick = (element) => {
-        console.log("Handling click");
-
-        switch (element) {
-            case "C":
-                resetClickHandler();
-                break;
-            case "+-":
-                invertClickHandler();
-                break;
-            case "%":
-                percentClickHandler();
-                break;
-            case "=":
-                equalClickHandler();
-                break;
-            case ".":
-                decClickHandler();
-                break;
-            case "+":
-            case "-":
-            case "X":
-            case "/":
-                operClickHandler(element);
-                break;
-            default:
-                numClickHandler(element);
+            return newOperand;
         }
 
-        console.log("Click finished!");
-    };
+        const resetClickHandler = () => {
+            console.log("\tCalculator reset");
+            this.setState({ screenVal: 0 });
+            this.operand = 0;
+            this.operator = "";
+            this.resultShowing = true;
+        };
 
-    return (
-        <div className="wrapper bg-dark-gray pa3 tc">
-            <Screen value={screenVal} />
-            <ButtonBox handleClick={onButtonClick} />
-        </div>
-    );
+        const invertClickHandler = () => {
+            console.log("\tnumber inverted");
+            this.setState({ screenVal: this.state.screenVal * -1 });
+        };
 
+        const percentClickHandler = () => {
+            console.log("\tconverting to percent");
+            this.setState({ screenVal: this.state.screenVal / 100 });
+
+        };
+
+        const equalClickHandler = () => {
+            console.log("\tCalculating " + this.operand + " " + this.operator + " " + this.state.screenVal);
+
+            if (!this.resultShowing) {
+                let displayVal = this.operator === "" ? this.state.screenVal : updateOperand();
+                this.setState({ screenVal: displayVal });
+                this.resultShowing = true;
+            }
+            this.operator = "";
+            this.operand = 0;
+        };
+
+        const decClickHandler = () => {
+            console.log("\tMaking number a decimal if not decimal already");
+            if (this.state.screenVal % 1 === 0) {
+                let screenValStr = this.state.screenVal.toString() + ".0";
+                this.decimalStart = true;
+                this.setState({ screenVal: Number(screenValStr) });
+            }
+        };
+
+        const operClickHandler = (oper) => {
+            console.log("\tHandling " + oper + " operator");
+
+            if (!this.resultShowing) {
+                let displayVal = this.operator === "" ? 0 : updateOperand();
+                this.setState({ screenVal: displayVal });
+                this.resultShowing = true;
+                this.operator = oper;
+
+                if (this.operand === 0) {
+                    this.operand = this.state.screenVal;
+                } else {
+                    this.operand = displayVal;
+                }
+            }
+        };
+
+        const numClickHandler = (num) => {
+            console.log("\t" + num + " clicked")
+            let screenValStr = this.state.screenVal.toString();
+            console.log("current num:" + screenValStr);
+            //directly after the decimal button is pressed
+            if (this.decimalStart) {
+                screenValStr += "."
+                console.log(screenValStr + num);
+                this.setState({ screenVal: Number(screenValStr + num) });
+                console.log("\tadding to num");
+                this.decimalStart = false;
+                this.resultShowing = false;
+            } else {
+                if (this.resultShowing) {
+                    //setting first num
+                    console.log("\tstarting new number");
+                    this.setState({ screenVal: Number(num) });
+                    this.resultShowing = false;
+                } else {
+                    //appending num
+                    console.log("\tappending to existing num");
+                    this.setState({ screenVal: Number(screenValStr + num) });
+                }
+            }
+        };
+
+        const onButtonClick = (element) => {
+            console.log("Handling click");
+
+            switch (element) {
+                case "C":
+                    resetClickHandler();
+                    break;
+                case "+-":
+                    invertClickHandler();
+                    break;
+                case "%":
+                    percentClickHandler();
+                    break;
+                case "=":
+                    equalClickHandler();
+                    break;
+                case ".":
+                    decClickHandler();
+                    break;
+                case "+":
+                case "-":
+                case "X":
+                case "/":
+                    operClickHandler(element);
+                    break;
+                default:
+                    numClickHandler(element);
+            }
+
+            console.log("Click finished!");
+        };
+
+        return (
+            <div className="wrapper bg-dark-gray pa3 tc" >
+                <Screen value={this.decimalStart ? this.state.screenVal + "." : this.state.screenVal} />
+                <ButtonBox handleClick={onButtonClick} />
+            </div>
+        );
+    }
 };
 
 export default Wrapper;
